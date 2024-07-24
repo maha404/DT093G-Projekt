4962ad6b81d3a5b72da0e030cfcb8e6d171c6457
Your Name <you@example.com>
2024-07-24 13:30:37 +0200

    Added if statement to check if array is empty

--- Projekt/index.php
+++ Projekt/index.php
@@ -6,25 +6,33 @@
 <?php 
     $blog = new BlogPost(); 
     $array = $blog->getAllPost(); // Hämtar de 5 senaste inläggen som finns i databasen. 
-    $lastItem = $array[array_key_first($array)]; // Tar det senaste inlägget i arrayen. 
-    $partof = substr($lastItem['content'], 0, 500);
+
+    if ($array != []) {
+        $lastItem = $array[array_key_first($array)]; // Tar det senaste inlägget i arrayen. 
+        $partof = substr($lastItem['content'], 0, 500);
+    ?>
+        <div class="top_container" >
+            <article class="top_article">
+
+                <img class="top_img" src="photos/<?= $lastItem['image_url']?>" alt="">
+
+                <div class="top_article_background">
+                    <h2><?= $lastItem['title'] ?></h2>
+                    <h3><?= $lastItem['time'] ?></h3>
+                    <p>Av: <?= $lastItem['name']?></p>
+                    <p><?= $partof?>...</p>
+                    <a href='article.php?id=<?= $lastItem['post_id']; ?>'>Läs mer &#8594; </a>
+                </div>
+                
+            </article>
+        </div>
+<?php
+    }
 ?>
 
-<div class="top_container" >
-<article class="top_article">
+<?php
 
-    <img class="top_img" src="photos/<?= $lastItem['image_url']?>" alt="">
-
-    <div class="top_article_background">
-        <h2><?= $lastItem['title'] ?></h2>
-        <h3><?= $lastItem['time'] ?></h3>
-        <p>Av: <?= $lastItem['name']?></p>
-        <p><?= $partof?>...</p>
-        <a href='article.php?id=<?= $lastItem['post_id']; ?>'>Läs mer &#8594; </a>
-    </div>
-    
-</article>
-</div>
+?>
 
 <div class="home_container">
 <?php
@@ -32,22 +40,29 @@
 
     $array = array_slice($blog->getAllPost(), 1); // Hämtar endast från första array index till sista. 
 
-     foreach($array as $post) { // Skriver ut 4 poster. 
-        $partof = substr($post['content'], 0, 500);
-        ?> 
-        <article class="post_article">
-            <img class="post_img" src="photos/<?= $post['image_url']?>" alt="">
-            <div class="post_background">
-                <h2><?= $post['title']; ?></h2>
-                <h3><?= $post['time']; ?></h3>
-                <p>Av: <?= $post['name']; ?></p>
-                <p><?= $partof; ?>... </p>
-                <a href='article.php?id=<?= $post['post_id']; ?>'>Läs mer &#8594; </a>
-            </div>
-        </article> 
-    
-        <?php
-        }
+    if($array === []) {
+        
+    } else {
+        foreach($array as $post) { // Skriver ut 4 poster. 
+            $partof = substr($post['content'], 0, 500);
+            ?> 
+            <article class="post_article">
+                <img class="post_img" src="photos/<?= $post['image_url']?>" alt="">
+                <div class="post_background">
+                    <h2><?= $post['title']; ?></h2>
+                    <h3><?= $post['time']; ?></h3>
+                    <p>Av: <?= $post['name']; ?></p>
+                    <p><?= $partof; ?>... </p>
+                    <a href='article.php?id=<?= $post['post_id']; ?>'>Läs mer &#8594; </a>
+                </div>
+            </article> 
+        
+            <?php
+            }
+    }
+
+
+     
     ?>
 </div>
 
